379d0fc1a45bccf664868ff39136151462d67410
aryaneferreira <0hoppe0@gmail.com>
2024-07-04 00:56:18 -0300

    criar estrutura do projeto

---
+++ src/test/java/com/proj/saudemvp/SaudeMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.saudemvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaudeMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
